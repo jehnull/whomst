54a92c4358854d094fd421d68dd3aac61a47bae5
juhnull <jehnull28@gmail.com>
2022-12-26 00:27:01 -0800

    idek test of "hover" cards

--- whomst-app/src/App.js
+++ whomst-app/src/App.js
@@ -1,26 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import images from './images';
+import React, { useState } from 'react';
+import {shuffle} from 'lodash';
 
 function App() {
+  const [cards,setCards] = useState( shuffle([...images]) );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          ADDING THIS IN TO SEE IF MAKING CHANGES WORK AND SHOW UP ON NETLIFY !! 🎄✨
-        </h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React. no.
-        </a>
-      </header>
+    <div>
+      <h1>WHOMST</h1>
+      <div className="board">
+        {cards.map((card,index) => {
+          return (
+            <div className={"card-outer"}>
+              <div className="card">
+                <div className="front">
+                  <img id="pic" src={card} alt=""/>
+                </div>
+                <div className="back">
+                  
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
     </div>
   );
 }
---
+++ whomst-app/src/images.js
@@ -0,0 +1,17 @@
+import anthony from './my_pics/anthony.PNG';
+import demi from './my_pics/demi.PNG';
+import kevin from './my_pics/kevin.PNG';
+import luigi from './my_pics/luigi.PNG';
+import olivia from './my_pics/olivia.PNG';
+import puttputt from './my_pics/puttputt.PNG';
+import rabbidpeach from './my_pics/rabbidpeach.PNG';
+import raquelle from './my_pics/raquelle.PNG';
+import reagan from './my_pics/reagan.PNG';
+import rodrick from './my_pics/rodrick.PNG';
+import simon from './my_pics/simon.PNG';
+import spencer from './my_pics/spencer.PNG';
+import tom from './my_pics/tom.PNG';
+import vendetta from './my_pics/vendetta.PNG';
+import wybie from './my_pics/wybie.PNG';
+
+export default [anthony, demi, kevin, luigi, olivia, puttputt, rabbidpeach, raquelle, reagan, rodrick, simon, spencer, tom, vendetta, wybie];

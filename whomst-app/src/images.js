beba903c0dbcaf4bba177348ecc01af5edfc209e
juhnull <jehnull28@gmail.com>
2022-12-26 14:18:10 -0800

    testing - add bg color based on device size

--- whomst-app/src/images.js
+++ whomst-app/src/images.js
@@ -14,8 +14,8 @@ import tom from './my_pics/tom.PNG';
 import vendetta from './my_pics/vendetta.PNG';
 import wybie from './my_pics/wybie.PNG';
 
-const picArr = {
+const picArr = [
     anthony, demi, kevin, luigi, olivia, puttputt, rabbidpeach, raquelle, reagan, rodrick, simon, spencer, tom, vendetta, wybie
-};
+];
 
 export default picArr;

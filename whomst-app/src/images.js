d30aec64fbc4e9603f3b8da8b2d8460533ab1544
juhnull <jehnull28@gmail.com>
2022-12-29 13:16:28 -0800

    prototype i don't like - not centered odd #

--- whomst-app/src/images.js
+++ whomst-app/src/images.js
@@ -1,18 +1,35 @@
-import anthony from './my_pics/anthonyc.PNG';
-import demi from './my_pics/demic.PNG';
-import kevin from './my_pics/kevinc.PNG';
-import luigi from './my_pics/luigic.PNG';
-import olivia from './my_pics/oliviac.PNG';
-import puttputt from './my_pics/puttputtc.PNG';
-import rabbidpeach from './my_pics/rabbidpeachc.PNG';
-import raquelle from './my_pics/raquellec.PNG';
-import reagan from './my_pics/reaganc.PNG';
-import rodrick from './my_pics/rodrickc.PNG';
-import simon from './my_pics/simonc.PNG';
-import spencer from './my_pics/spencerc.PNG';
-import tom from './my_pics/tomc.PNG';
-import vendetta from './my_pics/vendettac.PNG';
-import wybie from './my_pics/wybiec.PNG';
+// import anthony from './my_pics/rabbid.PNG';
+// import demi from './my_pics/demic.PNG';
+// import kevin from './my_pics/kevinc.PNG';
+// import luigi from './my_pics/luigic.PNG';
+// import olivia from './my_pics/oliviac.PNG';
+// import puttputt from './my_pics/puttputtc.PNG';
+// import rabbidpeach from './my_pics/rabbidpeachc.PNG';
+// import raquelle from './my_pics/raquellec.PNG';
+// import reagan from './my_pics/reaganc.PNG';
+// import rodrick from './my_pics/rodrickc.PNG';
+// import simon from './my_pics/simonc.PNG';
+// import spencer from './my_pics/spencerc.PNG';
+// import tom from './my_pics/tomc.PNG';
+// import vendetta from './my_pics/vendettac.PNG';
+// import wybie from './my_pics/wybiec.PNG';
+
+
+import anthony from './my_pics/rabbidPeachCard.PNG';
+import demi from './my_pics/rabbidPeachCard.PNG';
+import kevin from './my_pics/rabbidPeachCard.PNG';
+import luigi from './my_pics/rabbidPeachCard.PNG';
+import olivia from './my_pics/rabbidPeachCard.PNG';
+import puttputt from './my_pics/rabbidPeachCard.PNG';
+import rabbidpeach from './my_pics/rabbidPeachCard.PNG';
+import raquelle from './my_pics/rabbidPeachCard.PNG';
+import reagan from './my_pics/rabbidPeachCard.PNG';
+import rodrick from './my_pics/rabbidPeachCard.PNG';
+import simon from './my_pics/rabbidPeachCard.PNG';
+import spencer from './my_pics/rabbidPeachCard.PNG';
+import tom from './my_pics/rabbidPeachCard.PNG';
+import vendetta from './my_pics/rabbidPeachCard.PNG';
+import wybie from './my_pics/rabbidPeachCard.PNG';
 
 const picArr = [
     anthony, demi, kevin, luigi, olivia, puttputt, rabbidpeach, raquelle, reagan, rodrick, simon, spencer, tom, vendetta, wybie
